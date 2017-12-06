66701168a85f1330616a6979b686d25441cb52df
Manuel Jesús De la Cruz Sotelo <manuel7851@gmail.com>
2017-12-06 01:44:00 -0500

    ADD publications admin

--- apps/publications/admin.py
+++ apps/publications/admin.py
@@ -9,6 +9,9 @@ from django.contrib import admin
 
 
 # Local imports
+from .models import Publication, PublicationKind
 
 
 # Register your models here.
+admin.site.register(Publication)
+admin.site.register(PublicationKind)
